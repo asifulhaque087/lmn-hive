2f28fc08078e64931a16d2bdd27f3cc65ab71398
asiful <asiful@pop-os.localdomain>
2022-08-05 17:35:00 +0600

    add button and modify hero-section

--- pages/index.js
+++ pages/index.js
@@ -30,41 +30,34 @@ export default function Home() {
         {/* right */}
       </div>
 
-      {/* hero section */}
-      {/* <div className="px-5">
-        <h1 className="text-[48px] md:text-[100px] leading-[58px]  md:leading-[120px] text-[#0A142F] font-bold text-right">
-          React
-          <br />
-          Conference
-        </h1>
-      </div> */}
-
       <div className="grid place-items-center px-[10%] mt-[20%]">
-        {/* <div className="grid grid-cols-1 md:grid-cols-3"> */}
         <div className="flex flex-col items-start md:flex-row gap-8 md:items-center">
           {/* left */}
-          <div className="order-last md:order-first">
-            <div className="w-full  max-w-[327px]">
-              <img
-                src="/hero1.jpg"
-                className="w-full rounded-[30px]"
-                alt="hero1"
-              />
-            </div>
+          <div className="order-last md:order-first w-full  max-w-[250px] md:mt-40">
+            <img
+              src="/hero1.jpg"
+              className="w-full rounded-[30px]"
+              alt="hero1"
+            />
           </div>
           {/* middle */}
 
-          <div className="relative md:mb-32">
-            <p className="w-full  max-w-[550px] md:max-w-full">
+          <div className="relative md:mb-5">
+            <p className="text-sm  opacity-[0.8] w-full  max-w-[550px] md:max-w-full">
               Lorem uis diam turpis quam id fermetum.In quis diam turpis quam id
               fermentu me.In quis diam turpis quam id fermentum.
             </p>
-            <button>
-              Buy Tickets <span>-</span>
-            </button>
+            <div className="grid place-items-center">
+              <button className="mt-3 bg-main-yellow  mx-auto px-12 py-3 rounded-full flex items-center font-medium">
+                <span>Buy Tickets</span>
+                <span className="ml-2">
+                  <img src="/buttonIcon.png" alt="button" />
+                </span>
+              </button>
+            </div>
 
-            <div className="px-5 absolute  bottom-full md:right-0">
-              <h1 className="text-[48px] md:text-[100px] leading-[58px]  md:leading-[120px] text-[#0A142F] font-bold text-right">
+            <div className="absolute  bottom-[140%] md:right-0">
+              <h1 className="text-[48px] md:text-[88px] leading-[58px]  md:leading-[85px] text-[#0A142F] font-bold text-right">
                 React
                 <br />
                 Conference
@@ -73,17 +66,15 @@ export default function Home() {
           </div>
           {/* right */}
 
-          <div>
-            <div className="relative w-full  max-w-[550px]">
-              <img
-                src="/hero2.jpg"
-                className="w-full rounded-[50px]"
-                alt="hero2"
-              />
+          <div className="relative w-full  max-w-[550px] md:mt-20">
+            <img
+              src="/hero2.jpg"
+              className="w-full rounded-[50px]"
+              alt="hero2"
+            />
 
-              <div className="absolute right-[-10px] md:left-0 bottom-[-55px]">
-                <img src="/star.png" alt="star" />
-              </div>
+            <div className="absolute right-[-10px] md:left-0 bottom-[-55px]">
+              <img src="/star.png" alt="star" />
             </div>
           </div>
         </div>
