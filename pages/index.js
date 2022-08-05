fa4677f41a44f06a9dbbc7e112a258b3ae43950a
asiful <asiful@pop-os.localdomain>
2022-08-05 11:30:11 +0600

    change order

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,46 @@ export default function Home() {
     <>
       <div className="l-g"></div>
       <div className="r-g"></div>
+      {/* hero section */}
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        {/* left */}
+
+        <div className="order-last md:order-first">
+          <div>
+            <Image
+              className="rounded-[20px]"
+              src="/hero1.jpg"
+              alt="me"
+              width={157}
+              height={200}
+            />
+          </div>
+        </div>
+        {/* middle */}
+
+        <div>
+          <p>
+            Lorem uis diam turpis quam id fermetum.In quis diam turpis quam id
+            fermentu me.In quis diam turpis quam id fermentum.
+          </p>
+          <button>
+            Buy Tickets <span>-</span>
+          </button>
+        </div>
+        {/* right */}
+
+        <div>
+          <div>
+            <Image
+              className="rounded-[35px]"
+              src="/hero2.jpg"
+              alt="me"
+              width={327}
+              height={333}
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
