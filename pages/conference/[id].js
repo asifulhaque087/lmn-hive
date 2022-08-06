5fde8406f91160d8bb947fadb8ad00a6ee819aa8
asiful <asiful@pop-os.localdomain>
2022-08-06 12:55:45 +0600

    responsive conference page

--- pages/conference/[id].js
+++ pages/conference/[id].js
@@ -1,14 +1,15 @@
 import { gql } from "@apollo/client";
+import { useState } from "react";
 import client from "../../graphql/client";
 
 const Conference = ({ conference }) => {
-  console.log(conference);
   return (
     <div>
       <div className="flex mt-40 gap-10 px-[10%]">
         {/* left */}
+        <div className="w-full md:w-[450px]">
+          {/* items */}
 
-        <div className="w-[450px]">
           <div className="mb-5 flex gap-5  p-2 border rounded items-center">
             <div className="bg-main-yellow/[.15] p-2 rounded">
               <img src="../cInfo.png" className="w-[20px] " alt="" />
@@ -16,11 +17,56 @@ const Conference = ({ conference }) => {
             <p className="text-sm font-bold">Organizer</p>
           </div>
 
-          <div className="mb-5 flex gap-5 bg-main-yellow  p-2 border rounded items-center">
-            <div className="bg-white p-2 rounded">
-              <img src="../cInfo.png" className="w-[20px] " alt="" />
+          {/* {
+            conference.conference
+          } */}
+
+          <div className="">
+            <div className="mb-5 flex gap-5 bg-main-yellow  p-2 border rounded items-center">
+              <div className="bg-white p-2 rounded">
+                <img src="../cInfo.png" className="w-[20px] " alt="" />
+              </div>
+              <p className="text-sm font-bold">Speakers</p>
             </div>
-            <p className="text-sm font-bold">Speakers</p>
+
+            <div className="md:hidden p-10 bg-[#FBFBFB] text-[#0A142F] rounded">
+              {/* loop here */}
+              {conference &&
+                conference.conference &&
+                conference.conference.speakers &&
+                conference.conference.speakers.map((sp, i) => {
+                  return (
+                    <div
+                      key={i}
+                      className="p-4 mb-5 flex    items-center bg-white"
+                    >
+                      {/* left */}
+                      <div className="w-[50%] sm:w-[20%]">
+                        <img
+                          className="rounded-md w-full"
+                          src={sp.image.url}
+                          alt={sp.image.title}
+                        />
+                      </div>
+                      {/* right */}
+                      <div className="px-5 w-[50%] sm:w-[80%]">
+                        <div className="flex flex-col sm:flex-row justify-between md:items-center text-sm font-bold mb-3 pr-3">
+                          <span>
+                            {sp.firstName} {sp.lastName}
+                          </span>
+                          <span className="font-normal opacity-[0.5]">
+                            {sp.company}
+                          </span>
+                        </div>
+
+                        <p className="text-sm  opacity-[0.8]">
+                          {sp.aboutShort}
+                        </p>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
           </div>
 
           <div className="mb-5 flex gap-5  p-2 border rounded items-center">
@@ -46,27 +92,24 @@ const Conference = ({ conference }) => {
         </div>
 
         {/* right */}
-        <div className="p-10 bg-[#FBFBFB] text-[#0A142F] rounded">
+        <div className="hidden w-full md:block p-10 bg-[#FBFBFB] text-[#0A142F] rounded">
           {/* loop here */}
           {conference &&
             conference.conference &&
             conference.conference.speakers &&
-            conference.conference.speakers.map((sp) => {
+            conference.conference.speakers.map((sp, i) => {
               return (
-                <div
-                  key={sp.id}
-                  className="p-4 mb-5 flex  items-center bg-white"
-                >
+                <div key={i} className="p-4 mb-5 flex  items-center bg-white">
                   {/* left */}
-                  <div>
+                  <div className="w-[20%]">
                     <img
-                      className="rounded-md w-[200px]"
+                      className="rounded-md w-full"
                       src={sp.image.url}
                       alt={sp.image.title}
                     />
                   </div>
                   {/* right */}
-                  <div className="px-5">
+                  <div className="px-5 w-[80%]">
                     <div className="flex justify-between items-center text-sm font-bold mb-3 pr-3">
                       <span>
                         {sp.firstName} {sp.lastName}
@@ -76,11 +119,7 @@ const Conference = ({ conference }) => {
                       </span>
                     </div>
 
-                    <p className="text-sm  opacity-[0.8]">
-                      Lorem ipsum dolor sit amet, conse ctetur adip iscing elit.
-                      Et condi mentum lectus in vel pel lent esque arcu non
-                      odio. Ut dis eu dolor ac tellus vitae ut.
-                    </p>
+                    <p className="text-sm  opacity-[0.8]">{sp.aboutShort}</p>
                   </div>
                 </div>
               );
